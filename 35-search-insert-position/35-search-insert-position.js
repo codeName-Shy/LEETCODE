4b8ee69f00cfc5727371cb215677b47112f2add7
Sharmae_Dev <94461537+codeName-Shy@users.noreply.github.com>
2022-02-16 22:59:24 +0800

    Time: 95 ms (42.01%), Space: 42.6 MB (5.21%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -6,10 +6,8 @@
 var searchInsert = function(nums, target) {
     if (nums.indexOf(target) >-1){
         return nums.indexOf(target)
-        // console.log(nums.indexOf(target))
 
     } else if(nums.indexOf(target) < 0){
-        // console.log(nums.concat(target).sort(function(a, b){return a-b})) 
         return nums.concat(target).sort(function(a, b){return a-b}).indexOf(target)
     }
 };

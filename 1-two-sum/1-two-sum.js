15b85a4526e925ce1fd9355910f1939add81ede4
Sharmae_Dev <94461537+codeName-Shy@users.noreply.github.com>
2022-02-27 21:40:58 -1200

    Time: 64 ms (98.12%), Space: 43 MB (19.86%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -9,6 +9,7 @@ var twoSum = function(nums, target) {
         let currentNumber = nums[i];
         let findNumber = target - currentNumber;
         let index2 = previousValues[findNumber]
+         // if index2 is not null return the current index and the index of the findNumber from the nums
         if(index2 != null){
             return [index2,i]
         }

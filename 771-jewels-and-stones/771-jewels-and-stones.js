4737bbd0274cba3d394d7cb65beb2762c4c274b0
Sharmae_Dev <94461537+codeName-Shy@users.noreply.github.com>
2022-04-08 21:18:03 +1100

    Delete 771-jewels-and-stones directory

--- 771-jewels-and-stones/771-jewels-and-stones.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} jewels
- * @param {string} stones
- * @return {number}
- */
-var numJewelsInStones = function(jewels, stones) {
-    let rock = stones.split('')
-    return jewels.split('').map(jewel => rock.filter(stone=> stone === jewel).length).reduce((a,b) => a +b)
-};
